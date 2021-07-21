e21398645e10b0054f981df4bbceb9d3792f4ea0
Jeong Ga Hee <jghgahee@ajou.ac.kr>
2021-07-21 15:04:53 +0900

    data binding test

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
+const userName = "Ikanny!!!";
+
 const IndexPage = () => {
     return (
         <div>
-            <p>hello world</p>
+            <p>hello world! {userName}</p>
         </div>
     );
 }
 
 export default IndexPage;
+
+//hello world! Ikanny!!!
+//data binding
+//data를 UI bind 묶는다
