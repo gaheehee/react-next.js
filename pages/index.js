e8deeb11ef584e55319fcf33009dd9aac40b5000
Jeong Ga Hee <jghgahee@ajou.ac.kr>
2021-07-21 17:31:10 +0900

    router

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
+import Link from "next/link"
+import { useRouter } from "next/router";
+
 const userName = "Ikanny!!!";
 
 const IndexPage = () => {
+    const router = useRouter();
+    const onMoveClick = () => router.push("/friends")
     return (
         <div>
             <p>hello world! {userName}</p>
+            <Link href="/counter">
+            <a>Counter</a>
+            </Link>
+            <p> </p>
+            <button onClick={onMoveClick}>move</button>
         </div>
     );
 }
